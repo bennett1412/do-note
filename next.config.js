f7205191c9c46e4013325a4593d4f5ce027f43cc
Bennett B Madavana <34763279+bennett1412@users.noreply.github.com>
2023-05-04 18:20:45 +0530

    Added pwa config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -9,4 +14,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
@@ -15,12 +12,25 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#d7dede" />
         <meta name="theme-color" content="#d7dede" />
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+        {/*twitter meta tags*/}
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PWA App" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
